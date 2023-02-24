269e0d9bd70f431bfbdf3ec679837f68b9876431
afk111 <126279373+afkashnik111@users.noreply.github.com>
2023-02-24 20:35:58 +0300

    Add files via upload

---
+++ FirstOne.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstOne : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {   // типо пометка пишет привет
+        print("Hello world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
